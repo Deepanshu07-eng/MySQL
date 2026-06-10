c9298d8907eb57e1ab173d4c46ab37be0c1358b2
Deepanshu <d2004gautam@gmail.com>
2026-06-10 23:48:58 +0530

    Day 3 of learning SQL

---
+++ Day 2.sql
@@ -0,0 +1,6 @@
+USE startersql;
+
+-- INSERT INTO users (name, email, gender, date_of_birth) VALUES
+-- ('Kelvin', 'kelvin12@gmail.com', 'Male', '2000-12-30')
+
+SELECT * from users;
---
+++ Day 3.sql
@@ -0,0 +1,6 @@
+USE startersql;
+SELECT from users WHERE date_of_birth<'1995-09-09';
+SELECT from users WHERE date_of_birth BETWEEN '1990-09-09' AND '1999-09-09';
+SELECT from users WHERE gender in ('Male', 'Female')
+SELECT FROM users WHERE gender='Male' AND salary>'70000';
+SELECT * FROM users WHERE gender= 'Male' OR salary>'65000' ORDER BY date_of_birth DESC LIMIT 5;
