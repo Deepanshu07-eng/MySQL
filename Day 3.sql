15ddedba3fc35b82c0db58286459c4c2bb5fa6ae
Deepanshu <d2004gautam@gmail.com>
2026-06-10 23:51:56 +0530

    Reverting the code

--- Day 3.sql
+++ Day 3.sql
@@ -3,4 +3,4 @@ SELECT from users WHERE date_of_birth<'1995-09-09';
 SELECT from users WHERE date_of_birth BETWEEN '1990-09-09' AND '1999-09-09';
 SELECT from users WHERE gender in ('Male', 'Female')
 SELECT FROM users WHERE gender='Male' AND salary>'70000';
-SELECT id FROM users WHERE gender= 'Male' OR salary>'65000' ORDER BY date_of_birth DESC LIMIT 5;
+SELECT * FROM users WHERE gender= 'Male' OR salary>'65000' ORDER BY date_of_birth DESC LIMIT 5;
